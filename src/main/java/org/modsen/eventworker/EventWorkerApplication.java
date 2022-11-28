673636f8fee1e4fb2b74443c0daad539d3460a92
SunnyWorker <artem.khotin@gmail.com>
2022-11-28 15:00:48 +0300

    Initial commit

---
+++ src/main/java/org/modsen/eventworker/EventWorkerApplication.java
@@ -0,0 +1,13 @@
+package org.modsen.eventworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventWorkerApplication.class, args);
+    }
+
+}
